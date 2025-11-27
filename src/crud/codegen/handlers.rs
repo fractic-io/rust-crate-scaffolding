efa46499289dbbd0312e90435541d278cc440d3e
Mart van Buren <mart@fractic.io>
2025-11-27 18:05:51 +0900

    Initial CRUD handlers codegen logic.

---
+++ src/crud/codegen/handlers.rs
@@ -0,0 +1,242 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::Ident;
+
+use crate::{crud::model::ConfigModel, helpers::to_snake_case};
+
+pub fn generate(model: &ConfigModel) -> TokenStream {
+    let repo_name = &model.repository_name;
+    let repo_name_snake = to_snake_case(&repo_name.to_string());
+    let macro_name_ident = Ident::new(
+        &format!("generate_{}_handlers", repo_name_snake),
+        repo_name.span(),
+    );
+
+    // One result enum for the entire scaffolding.
+    let crud_result_enum = quote! {
+        #[derive(::serde::Serialize)]
+        #[serde(untagged)]
+        pub enum __CrudOperationResult<T>
+        where
+            T: ::fractic_aws_dynamo::DynamoObject + ::serde::Serialize,
+        {
+            Created { created_id: ::fractic_aws_dynamo::schema::PkSk },
+            Read(T),
+            Unit(()),
+        }
+    };
+
+    // Build handler for a root type.
+    let root_handlers = model.root_objects.iter().map(|root| {
+        let ty_ident = &root.name;
+        let manager_ident = method_ident_for("manage", ty_ident);
+        let handler_ident = method_ident_for_with_suffix("manage", ty_ident, "_handler");
+        let has_children = root.has_children();
+
+        let delete_invoke = if has_children {
+            quote! { __repo.#manager_ident().delete_recursive(__item).await?; }
+        } else {
+            quote! { __repo.#manager_ident().delete(__item).await?; }
+        };
+
+        quote! {
+            pub async fn #handler_ident(
+                operation: ::fractic_aws_apigateway::CrudOperation<#ty_ident>
+            ) -> ::std::result::Result<__CrudOperationResult<#ty_ident>, ::fractic_server_error::ServerError> {
+                use ::fractic_aws_apigateway::CrudOperation::*;
+                let __repo: ::std::sync::Arc<dyn #repo_name> = { __repo_init!() };
+                match operation {
+                    Create { parent_id, after, data } => {
+                        if parent_id.is_some() {
+                            return ::std::result::Result::Err(
+                                ::fractic_aws_apigateway::InvalidCrudRequestParameters::new(
+                                    &format!("create operations on {} do not allow a parent ID", stringify!(#ty_ident))
+                                ).into()
+                            );
+                        }
+                        if after.is_some() {
+                            return ::std::result::Result::Err(
+                                ::fractic_aws_apigateway::InvalidCrudRequestParameters::new(
+                                    &format!("create operations on {} do not allow an `after` parameter", stringify!(#ty_ident))
+                                ).into()
+                            );
+                        }
+                        let __created = __repo.#manager_ident().add(data).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Created { created_id: __created.id })
+                    }
+                    Read { id } => {
+                        let __item = __repo.#manager_ident().get(id).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Read(__item))
+                    }
+                    Update { item } => {
+                        __repo.#manager_ident().update(&item).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Unit(()))
+                    }
+                    Delete { id } => {
+                        let __item = __repo.#manager_ident().get(id).await?;
+                        #delete_invoke
+                        ::std::result::Result::Ok(__CrudOperationResult::Unit(()))
+                    }
+                }
+            }
+        }
+    });
+
+    // Build handlers for ordered children (require parent_id, allow `after`).
+    let ordered_handlers = model.ordered_objects.iter().map(|child| {
+        let ty_ident = &child.name;
+        let parent_ident = &child.parents[0];
+        let parent_data_ident = Ident::new(&format!("{}Data", parent_ident), parent_ident.span());
+        let ty_data_ident = Ident::new(&format!("{}Data", ty_ident), ty_ident.span());
+        let manager_ident = method_ident_for("manage", ty_ident);
+        let handler_ident = method_ident_for_with_suffix("manage", ty_ident, "_handler");
+        let has_children = child.has_children();
+
+        let delete_invoke = if has_children {
+            quote! { __repo.#manager_ident().delete_recursive(__item).await?; }
+        } else {
+            quote! { __repo.#manager_ident().delete(__item).await?; }
+        };
+
+        quote! {
+            pub async fn #handler_ident(
+                operation: ::fractic_aws_apigateway::CrudOperation<#ty_ident>
+            ) -> ::std::result::Result<__CrudOperationResult<#ty_ident>, ::fractic_server_error::ServerError> {
+                use ::fractic_aws_apigateway::CrudOperation::*;
+                let __repo: ::std::sync::Arc<dyn #repo_name> = { __repo_init!() };
+                match operation {
+                    Create { parent_id, after, data } => {
+                        let Some(parent_id) = parent_id else {
+                            return ::std::result::Result::Err(
+                                ::fractic_aws_apigateway::InvalidCrudRequestParameters::new(
+                                    &format!("create operations on {} require a valid parent ID", stringify!(#ty_ident))
+                                ).into()
+                            );
+                        };
+                        let __tmp_parent = #parent_ident {
+                            id: parent_id,
+                            data: #parent_data_ident::default(),
+                            auto_fields: ::fractic_aws_dynamo::schema::AutoFields::default(),
+                        };
+                        let mut __tmp_after: ::std::option::Option<#ty_ident> = after.map(|id| #ty_ident {
+                            id,
+                            data: #ty_data_ident::default(),
+                            auto_fields: ::fractic_aws_dynamo::schema::AutoFields::default(),
+                        });
+                        let __after_ref: ::std::option::Option<& #ty_ident> = __tmp_after.as_ref();
+                        let __created = __repo.#manager_ident().add(&__tmp_parent, data, __after_ref).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Created { created_id: __created.id })
+                    }
+                    Read { id } => {
+                        let __item = __repo.#manager_ident().get(id).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Read(__item))
+                    }
+                    Update { item } => {
+                        __repo.#manager_ident().update(&item).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Unit(()))
+                    }
+                    Delete { id } => {
+                        let __item = __repo.#manager_ident().get(id).await?;
+                        #delete_invoke
+                        ::std::result::Result::Ok(__CrudOperationResult::Unit(()))
+                    }
+                }
+            }
+        }
+    });
+
+    // Build handlers for unordered children (require parent_id, forbid `after`).
+    let unordered_handlers = model.unordered_objects.iter().map(|child| {
+        let ty_ident = &child.name;
+        let parent_ident = &child.parents[0];
+        let parent_data_ident = Ident::new(&format!("{}Data", parent_ident), parent_ident.span());
+        let manager_ident = method_ident_for("manage", ty_ident);
+        let handler_ident = method_ident_for_with_suffix("manage", ty_ident, "_handler");
+        let has_children = child.has_children();
+
+        let delete_invoke = if has_children {
+            quote! { __repo.#manager_ident().delete_recursive(__item).await?; }
+        } else {
+            quote! { __repo.#manager_ident().delete(__item).await?; }
+        };
+
+        quote! {
+            pub async fn #handler_ident(
+                operation: ::fractic_aws_apigateway::CrudOperation<#ty_ident>
+            ) -> ::std::result::Result<__CrudOperationResult<#ty_ident>, ::fractic_server_error::ServerError> {
+                use ::fractic_aws_apigateway::CrudOperation::*;
+                let __repo: ::std::sync::Arc<dyn #repo_name> = { __repo_init!() };
+                match operation {
+                    Create { parent_id, after, data } => {
+                        let Some(parent_id) = parent_id else {
+                            return ::std::result::Result::Err(
+                                ::fractic_aws_apigateway::InvalidCrudRequestParameters::new(
+                                    &format!("create operations on {} require a valid parent ID", stringify!(#ty_ident))
+                                ).into()
+                            );
+                        };
+                        if after.is_some() {
+                            return ::std::result::Result::Err(
+                                ::fractic_aws_apigateway::InvalidCrudRequestParameters::new(
+                                    &format!("create operations on {} do not allow an `after` parameter", stringify!(#ty_ident))
+                                ).into()
+                            );
+                        }
+                        let __tmp_parent = #parent_ident {
+                            id: parent_id,
+                            data: #parent_data_ident::default(),
+                            auto_fields: ::fractic_aws_dynamo::schema::AutoFields::default(),
+                        };
+                        let __created = __repo.#manager_ident().add(&__tmp_parent, data).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Created { created_id: __created.id })
+                    }
+                    Read { id } => {
+                        let __item = __repo.#manager_ident().get(id).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Read(__item))
+                    }
+                    Update { item } => {
+                        __repo.#manager_ident().update(&item).await?;
+                        ::std::result::Result::Ok(__CrudOperationResult::Unit(()))
+                    }
+                    Delete { id } => {
+                        let __item = __repo.#manager_ident().get(id).await?;
+                        #delete_invoke
+                        ::std::result::Result::Ok(__CrudOperationResult::Unit(()))
+                    }
+                }
+            }
+        }
+    });
+
+    // Compose the macro similar to repository handlers.
+    let root_handlers_iter = root_handlers.into_iter();
+    let ordered_handlers_iter = ordered_handlers.into_iter();
+    let unordered_handlers_iter = unordered_handlers.into_iter();
+    quote! {
+        #[allow(unused_macros)]
+        macro_rules! #macro_name_ident {
+            ($($repo_init:tt)+) => {
+                macro_rules! __repo_init { () => { { $($repo_init)+ } } }
+                #crud_result_enum
+                #(#root_handlers_iter)*
+                #(#ordered_handlers_iter)*
+                #(#unordered_handlers_iter)*
+            };
+        }
+
+        #[allow(unused_imports)]
+        pub(crate) use #macro_name_ident;
+    }
+}
+
+fn method_ident_for(prefix: &str, ident: &Ident) -> Ident {
+    let snake = to_snake_case(&ident.to_string());
+    let name = format!("{}_{}", prefix, snake);
+    Ident::new(&name, ident.span())
+}
+
+fn method_ident_for_with_suffix(prefix: &str, ident: &Ident, suffix: &str) -> Ident {
+    let snake = to_snake_case(&ident.to_string());
+    let name = format!("{}_{}{}", prefix, snake, suffix);
+    Ident::new(&name, ident.span())
+}
--- src/crud/mod.rs
+++ src/crud/mod.rs
@@ -4,6 +4,7 @@ use quote::quote;
 mod ast;
 mod codegen {
     pub mod annotations;
+    pub mod handlers;
     pub mod repository;
     pub mod repository_impl;
 }
@@ -16,9 +17,11 @@ pub fn generate(model: &ConfigModel) -> TokenStream {
     let repository_trait = codegen::repository::generate(model);
     let repository_impl_macro = codegen::repository_impl::generate(model);
     let annotations_macro = codegen::annotations::generate(model);
+    let handlers_macro = codegen::handlers::generate(model);
     quote! {
         #repository_trait
         #repository_impl_macro
         #annotations_macro
+        #handlers_macro
     }
 }
